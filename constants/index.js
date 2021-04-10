5241be1394591f69a550cf956eefa44432475764
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-10 21:59:13 +0530

    constants

---
+++ constants/index.js
@@ -0,0 +1,546 @@
+const FONTS = [
+  {
+    text: { type: "plain_text", text: "Anonymous Pro" },
+    value: "Anonymous Pro",
+  },
+  {
+    text: { type: "plain_text", text: "Droid Sans Mono" },
+    value: "Droid Sans Mono",
+  },
+  {
+    text: { type: "plain_text", text: "Fantasque Sans Mono" },
+    value: "Fantasque Sans Mono",
+  },
+  { text: { type: "plain_text", text: "Fira Code" }, value: "Fira Code" },
+  { text: { type: "plain_text", text: "Hack" }, value: "Hack" },
+  {
+    text: { type: "plain_text", text: "IBM Plex Mono" },
+    value: "IBM Plex Mono",
+  },
+  { text: { type: "plain_text", text: "Inconsolata" }, value: "Inconsolata" },
+  { text: { type: "plain_text", text: "Iosevka" }, value: "Iosevka" },
+  {
+    text: { type: "plain_text", text: "JetBrains Mono" },
+    value: "JetBrains Mono",
+  },
+  { text: { type: "plain_text", text: "Monoid" }, value: "Monoid" },
+  {
+    text: { type: "plain_text", text: "Source Code Pro" },
+    value: "Source Code Pro",
+  },
+  { text: { type: "plain_text", text: "Space Mono" }, value: "Space Mono" },
+  { text: { type: "plain_text", text: "Ubuntu Mono" }, value: "Ubuntu Mono" },
+];
+
+// needs revision
+export const THEMES = [
+  {
+    text: {
+      type: "plain_text",
+      text: "3024 Night",
+    },
+    value: "3024 Night",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "A11y Dark",
+    },
+    value: "A11y Dark",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Blackboard",
+    },
+    value: "Blackboard",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Base 16 (Dark)",
+    },
+    value: "Base 16 (Dark)",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Base 16 (Light)",
+    },
+    value: "Base 16 (Light)",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Cobalt",
+    },
+    value: "Cobalt",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Dracula",
+    },
+    value: "Dracula",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Duotone",
+    },
+    value: "Duotone",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Hopscotch",
+    },
+    value: "Hopscotch",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Lucario",
+    },
+    value: "Lucario",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Material",
+    },
+    value: "Material",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Monokai",
+    },
+    value: "Monokai",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Night Owl",
+    },
+    value: "Night Owl",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Nord",
+    },
+    value: "Nord",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Oceanic Next",
+    },
+    value: "Oceanic Next",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "One Light",
+    },
+    value: "One Light",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "One Dark",
+    },
+    value: "One Dark",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Panda",
+    },
+    value: "Panda",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Paraiso",
+    },
+    value: "Paraiso",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Seti",
+    },
+    value: "Seti",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Shades of Purple ",
+    },
+    value: "Shades of Purple ",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Solarized (Dark)",
+    },
+    value: "Solarized (Dark)",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Solarized (Light)",
+    },
+    value: "Solarized (Light)",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "SynthWave '84",
+    },
+    value: "SynthWave '84",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Twilight",
+    },
+    value: "Twilight",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Verminal",
+    },
+    value: "Verminal",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "VSCode",
+    },
+    value: "VSCode",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Yeti",
+    },
+    value: "Yeti",
+  },
+  {
+    text: {
+      type: "plain_text",
+      text: "Zenburn",
+    },
+    value: "Zenburn",
+  },
+];
+
+// needs refactoring
+export const LANGUAGES = [
+  {
+    name: "Auto",
+    mode: "auto",
+  },
+  {
+    name: "Apache",
+    mode: "apache",
+  },
+  {
+    name: "Bash",
+    mode: "shell",
+  },
+  {
+    name: "Plain Text",
+    mode: "text",
+  },
+  {
+    name: "C",
+    mode: "clike",
+  },
+  {
+    name: "C++",
+    mode: "clike",
+  },
+  {
+    name: "C#",
+    mode: "clike",
+  },
+  {
+    name: "Clojure",
+    mode: "clojure",
+  },
+  {
+    name: "COBOL",
+    mode: "cobol",
+  },
+  {
+    name: "CoffeeScript",
+    mode: "coffeescript",
+  },
+  {
+    name: "Crystal",
+    mode: "crystal",
+  },
+  {
+    name: "CSS",
+    mode: "css",
+  },
+  {
+    name: "D",
+    mode: "d",
+  },
+  {
+    name: "Dart",
+    mode: "dart",
+  },
+  {
+    name: "Diff",
+    mode: "diff",
+  },
+  {
+    name: "Django",
+    mode: "django",
+  },
+  {
+    name: "Docker",
+    mode: "dockerfile",
+  },
+  {
+    name: "Elixir",
+    mode: "elixir",
+  },
+  {
+    name: "Elm",
+    mode: "elm",
+  },
+  {
+    name: "Erlang",
+    mode: "erlang",
+  },
+  {
+    name: "Fortran",
+    mode: "fortran",
+  },
+  {
+    name: "Gherkin",
+    mode: "gherkin",
+  },
+  {
+    name: "GraphQL",
+    mode: "graphql",
+  },
+  {
+    name: "Go",
+    mode: "go",
+  },
+  {
+    name: "Groovy",
+    mode: "groovy",
+  },
+  {
+    name: "Handlebars",
+    mode: "handlebars",
+  },
+  {
+    name: "Haskell",
+    mode: "haskell",
+  },
+  {
+    name: "HTML/XML",
+    mode: "htmlmixed",
+  },
+  {
+    name: "Java",
+    mode: "clike",
+  },
+  {
+    name: "JavaScript",
+    mode: "javascript",
+  },
+  {
+    name: "JSON",
+    mode: "javascript",
+  },
+  {
+    name: "JSX",
+    mode: "jsx",
+  },
+  {
+    name: "Julia",
+    mode: "julia",
+  },
+  {
+    name: "Kotlin",
+    mode: "clike",
+  },
+  {
+    name: "LaTeX",
+    mode: "stex",
+  },
+  {
+    name: "Lisp",
+    mode: "commonlisp",
+  },
+  {
+    name: "Lua",
+    mode: "lua",
+  },
+  {
+    name: "Markdown",
+    mode: "markdown",
+  },
+  {
+    name: "Mathematica",
+    mode: "mathematica",
+  },
+  {
+    name: "MATLAB/Octave",
+    mode: "octave",
+  },
+  {
+    name: "MySQL",
+    mode: "sql",
+  },
+  {
+    name: "N-Triples",
+    mode: "ntriples",
+  },
+  {
+    name: "NGINX",
+    mode: "nginx",
+  },
+  {
+    name: "Nim",
+    mode: "nim",
+  },
+  {
+    name: "Objective C",
+    mode: "clike",
+  },
+  {
+    name: "OCaml/F#",
+    mode: "mllike",
+  },
+  {
+    name: "Pascal",
+    mode: "pascal",
+  },
+  {
+    name: "Perl",
+    mode: "perl",
+  },
+  {
+    name: "PHP",
+    mode: "php",
+  },
+  {
+    name: "PowerShell",
+    mode: "powershell",
+  },
+  {
+    name: "Python",
+    mode: "python",
+  },
+  {
+    name: "R",
+    mode: "r",
+  },
+  {
+    name: "RISC-V",
+    mode: "riscv",
+  },
+  {
+    name: "Ruby",
+    mode: "ruby",
+  },
+  {
+    name: "Rust",
+    mode: "rust",
+  },
+  {
+    name: "Sass",
+    mode: "sass",
+  },
+  {
+    name: "Scala",
+    mode: "clike",
+  },
+  {
+    name: "Smalltalk",
+    mode: "smalltalk",
+  },
+  {
+    name: "Solidity",
+    mode: "solidity",
+  },
+  {
+    name: "SPARQL",
+    mode: "sparql",
+  },
+  {
+    name: "SQL",
+    mode: "sql",
+  },
+  {
+    name: "Stylus",
+    mode: "stylus",
+  },
+  {
+    name: "Swift",
+    mode: "swift",
+  },
+  {
+    name: "TCL",
+    mode: "tcl",
+  },
+  {
+    name: "TOML",
+    mode: "toml",
+  },
+  {
+    name: "Turtle",
+    mode: "turtle",
+  },
+  {
+    name: "TypeScript",
+    mode: "javascript",
+  },
+  {
+    name: "TSX",
+    mode: "jsx",
+  },
+  {
+    name: "Twig",
+    mode: "twig",
+  },
+  {
+    name: "VB.NET",
+    mode: "vb",
+  },
+  {
+    name: "Verilog",
+    mode: "verilog",
+  },
+  {
+    name: "VHDL",
+    mode: "vhdl",
+  },
+  {
+    name: "Vue",
+    mode: "vue",
+  },
+  {
+    name: "XQuery",
+    mode: "xquery",
+  },
+  {
+    name: "YAML",
+    mode: "yaml",
+  },
+];
+
+module.exports = { FONTS, THEMES };
