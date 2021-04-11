89c90b037f646ab79751eefa786f64b541a46e18
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-11 16:08:46 +0530

    get image

--- util/getImage.js
+++ util/getImage.js
@@ -1,15 +1,25 @@
-const { default: axios } = require("axios");
-const fs = require("fs");
+const { createWriteStream } = require("fs");
+const { pipeline } = require("stream");
+const { promisify } = require("util");
+const fetch = require("node-fetch");
+const { v4 } = require("uuid");
+const uploadFile = require("./uploadFile");
+require("dotenv").config();
 
-const getImage = async (data) => {
-  const response = await axios.post("https://carbonnowsh.herokuapp.com/", data);
+const getImage = async (body, client, userId) => {
+  let filename = {};
+  const streamPipeline = promisify(pipeline);
+  filename[userId] = `${v4()}.png`;
 
-  // fs.writeFile("test.png", response.data, "binary", (err) => {
-  //   if (err) throw err;
-  //   console.log("file saved");
-  // });
+  const response = await fetch("https://carbonnowsh.herokuapp.com/", {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  });
 
-  return response.data;
+  await streamPipeline(response.body, createWriteStream(filename[userId]));
+
+  return uploadFile(client, filename[userId]);
 };
 
 module.exports = getImage;
