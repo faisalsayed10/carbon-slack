4861cc0d2632415dd86402d5c29c8182e0906a69
faisalsayed10 <faisal.sayed502@gmail.com>
2021-04-11 16:08:17 +0530

    send message

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const { App } = require("@slack/bolt");
+const getImage = require("./util/getImage");
 const Modal = require("./views/modal");
+
+let channel = {};
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -9,15 +12,76 @@ const app = new App({
 
 app.command("/carbon", async ({ ack, body, client }) => {
   await ack();
+
+  channel[body.user_id] = body.channel_id;
+
   try {
-    const result = await client.views.open({
+    await client.views.open({
       trigger_id: body.trigger_id,
+      view_id: body.view_id,
       view: Modal,
     });
-    console.log(result);
   } catch (error) {
     console.error(error);
   }
+});
+
+app.view("modal_view_1", async ({ ack, view, client, body }) => {
+  await ack();
+  const values = view.state.values;
+  const message = values.message_input.message.value;
+  const code = values.code_input.code.value;
+  const backgroundColor = values.color_input.color.value;
+  const theme =
+    values.theme_input["theme_select-action"]?.selected_option?.value;
+  const language =
+    values.lang_input["language_select-action"]?.selected_option?.value;
+  const fontFamily =
+    values.ff_input["font_select-action"]?.selected_option?.value;
+
+  const data = { code, backgroundColor, language, theme, fontFamily };
+
+  const url = await getImage(
+    data,
+    client,
+    body.user.id,
+    channel[body.user.id],
+    message
+  );
+
+  await client.chat.postMessage({
+    token: process.env.SLACK_USER_TOKEN,
+    channel: channel[body.user.id],
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "plain_text",
+          text: message,
+          emoji: true,
+        },
+      },
+      {
+        type: "image",
+        image_url: url,
+        alt_text: "carbon_image",
+      },
+    ],
+  });
+
+  delete channel[body.user.id];
+});
+
+app.action("theme_select-action", async ({ ack }) => {
+  await ack();
+});
+
+app.action("font_select-action", async ({ ack }) => {
+  await ack();
+});
+
+app.action("language_select-action", async ({ ack }) => {
+  await ack();
 });
 
 (async () => {
