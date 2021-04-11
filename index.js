eadf0fb7dd5a6d197bfb4d0e3d65f6e0fcb9ca01
fayd <faisal.sayed502@gmail.com>
2021-04-11 13:50:18 +0000

    add context in response message

--- index.js
+++ index.js
@@ -7,83 +7,91 @@ const _ = require('lodash')
 let channel = {};
 
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
+	token: process.env.SLACK_BOT_TOKEN,
+	signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
 
 app.command("/carbon", async ({ ack, body, client }) => {
-  await ack();
+	await ack();
 
-  channel[body.user_id] = body.channel_id;
+	channel[body.user_id] = body.channel_id;
 
-  try {
-    await client.views.open({
-      trigger_id: body.trigger_id,
-      view_id: body.view_id,
-      view: Modal,
-    });
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		await client.views.open({
+			trigger_id: body.trigger_id,
+			view_id: body.view_id,
+			view: Modal,
+		});
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 app.view("modal_view_1", async ({ ack, view, client, body }) => {
-  await ack();
-  const values = view.state.values;
-  const message = values.message_input.message.value;
-  const code = encodeURI(values.code_input.code.value);
-  const backgroundColor = values.color_input.color.value;
-  // const theme = values.theme_input["theme_select-action"]?.selected_option?.value;
+	await ack();
+	const values = view.state.values;
+	const message = values.message_input.message.value;
+	const code = encodeURI(values.code_input.code.value);
+	const backgroundColor = values.color_input.color.value;
 	const theme = _.get(values, 'theme_input["theme_select-action"].selected_option.value')
-  const language = _.get(values, 'lang_input["language_select-action"].selected_option.value')
-  const fontFamily = _.get(values, 'ff_input["font_select-action"]?.selected_option?.value')
+	const language = _.get(values, 'lang_input["language_select-action"].selected_option.value')
+	const fontFamily = _.get(values, 'ff_input["font_select-action"]?.selected_option?.value')
 
-  const data = { code, backgroundColor, language, theme, fontFamily };
+	const data = { code, backgroundColor, language, theme, fontFamily };
 
-  const url = await getImage(
-    data,
-    client,
-    body.user.id,
-    channel[body.user.id],
-    message
-  );
+	const url = await getImage(
+		data,
+		client,
+		body.user.id,
+		channel[body.user.id],
+		message
+	);
 
-  await client.chat.postMessage({
-    token: process.env.SLACK_USER_TOKEN,
-    channel: channel[body.user.id],
-    blocks: [
-      {
-        type: "section",
-        text: {
-          type: "plain_text",
-          text: message,
-          emoji: true,
-        },
-      },
-      {
-        type: "image",
-        image_url: url,
-        alt_text: "carbon_image",
-      },
-    ],
-  });
+	await client.chat.postMessage({
+		token: process.env.SLACK_USER_TOKEN,
+		channel: channel[body.user.id],
+		blocks: [
+			{
+				type: "section",
+				text: {
+					type: "plain_text",
+					text: message,
+					emoji: true,
+				},
+			},
+			{
+				type: "image",
+				image_url: url,
+				alt_text: "carbon_image",
+			},
+			{
+			type: "context",
+			elements: [
+				{
+					type: "mrkdwn",
+					text: ":sparkles: Created with `/carbon`"
+				}
+			]
+		}
+		],
+	});
 
-  delete channel[body.user.id];
+	delete channel[body.user.id];
 });
 
 app.action("theme_select-action", async ({ ack }) => {
-  await ack();
+	await ack();
 });
 
 app.action("font_select-action", async ({ ack }) => {
-  await ack();
+	await ack();
 });
 
 app.action("language_select-action", async ({ ack }) => {
-  await ack();
+	await ack();
 });
 
 (async () => {
-  await app.start(process.env.PORT || 3000);
-  console.log("⚡️ Bolt app is running!");
+	await app.start(process.env.PORT || 3000);
+	console.log("⚡️ Bolt app is running!");
 })();
