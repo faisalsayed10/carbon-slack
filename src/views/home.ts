92632b4f321e8137964fd35e407c50134faf11f2
faisalsayed10 <faisal.sayed502@gmail.com>
2021-09-18 09:35:32 +0530

    Added private channel issue warning

--- src/views/home.ts
+++ src/views/home.ts
@@ -2,11 +2,12 @@ import { View } from "@slack/bolt";
 require("dotenv").config();
 
 export const helpText = `:wave: Hey! This is Carbon for Slack.
-It integrates the core functionality of <https://carbon.now.sh|carbon-app> into a slackbot and makes it possible for you to create and share beautiful images of your code directly in Slack.
+It integrates the core functionality of <https://carbon.now.sh|carbon-app> into a Slack bot and makes it possible for you to create and share beautiful images of your code directly in Slack.
 
 How to use it?
 
-1. Invoke the \`/carbon\` command (IMPORTANT: invoke the command only where you want to post your code because the image will be directly posted once you submit)
+1. Invoke the \`/carbon\` command (*IMPORTANT*: invoke the command only where you want to post your code because the image will be directly posted once you submit)
+*NOTE: Inorder to invoke this command in private channel, the bot must be added as a member of the channel.*
 
 2. Add your desired code, theme, font, background in the appropriate fields.
 
@@ -15,6 +16,9 @@ How to use it?
 4. Wait for a few seconds and voila!
 
 For more information, check out <https://carbon-slack.fayd.me|Carbon for Slack's Website>.`;
+
+export const errorText = `Hey, it seems like something went wrong and the command couldn't be invoked.
+If you're invoking this command in a *private channel*, make sure you've *added the bot* to that channel.`;
 
 export default {
 	type: "home",
@@ -33,7 +37,7 @@ export default {
 				},
 				{
 					type: "mrkdwn",
-					text: "This Slackbot is fully open-sourced at <https://github.com/faisalsayed10/carbon-slack/|faisalsayed10/carbon-slack>",
+					text: "This Slack bot is fully open-sourced at <https://github.com/faisalsayed10/carbon-slack/|faisalsayed10/carbon-slack>",
 				},
 			],
 		},
